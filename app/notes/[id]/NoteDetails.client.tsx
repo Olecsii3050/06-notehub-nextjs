2cb25c69c0ccb47fd7e1f6f42d8f9929e9fcfaae
Olecsii <vyriy3050@gmail.com>
2025-08-02 22:07:12 +0300

    06-notehub-nesxjs

---
+++ app/notes/[id]/NoteDetails.client.tsx
@@ -0,0 +1,38 @@
+import css from "./NoteDetails.module.css";
+import { useQuery } from "@tanstack/react-query";
+import { useParams } from "next/navigation";
+import { deleteNote } from "@/lib/api";
+
+const NoteDetailsClient = () => {
+  const { id } = useParams<{ id: string }>();
+
+  const noteId = parseInt(id, 10);
+
+  const {
+    data: note,
+    isLoading,
+    error,
+  } = useQuery({
+    queryKey: ["note", noteId],
+    queryFn: () => deleteNote(noteId),
+    refetchOnMount: false,
+  });
+
+  if (isLoading) return <p>Loading, please wait...</p>;
+
+  if (error || !note) return <p>Something went wrong.</p>;
+
+  return (
+    <div className={css.container}>
+      <div className={css.item}>
+        <div className={css.header}>
+          <h2>Note title: {note.title}</h2>
+        </div>
+        <p className={css.content}>Note content: {note.content}</p>
+        <p className={css.date}>Created date: {note.createdAt}</p>
+      </div>
+    </div>
+  );
+};
+
+export default NoteDetailsClient;
---
+++ app/notes/page.tsx
@@ -0,0 +1,9 @@
+const Notes = async () => {
+  return (
+    <section>
+      <h1>Notes List</h1>
+    </section>
+  );
+};
+
+export default Notes;
